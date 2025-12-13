0c0ec2f5414eb2ec1e3d495d381a8f9d753ae695
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2025-12-14 00:51:12 +0100

    Create client

---
+++ CauriousPotato.Clients/IIngredientsClient.cs
@@ -0,0 +1,11 @@
+﻿using CauriousPotato.Requests.Ingredients;
+using CautiousPotato.Core.Models;
+
+namespace CauriousPotato.Clients;
+
+public interface IIngredientsClient
+{
+    Task<Ingredient> CreateAsync(CreateIngredientRequest request);
+    Task<Ingredient[]> GetAllAsync();
+    Task<Ingredient?> GetAsync(GetIngredientRequest request);
+}
---
+++ CauriousPotato.Clients/IngredientsClient.cs
@@ -0,0 +1,28 @@
+﻿using CauriousPotato.Requests.Ingredients;
+using CautiousPotato.Core.Models;
+using Flurl;
+using System.Net.Http.Json;
+
+namespace CauriousPotato.Clients;
+
+public class IngredientsClient(HttpClient client) : IIngredientsClient
+{
+    private readonly Uri Path = new("/api/ingredients", UriKind.Relative);
+    public async Task<Ingredient> CreateAsync(CreateIngredientRequest request)
+    {
+        var response = await client.PostAsJsonAsync(Path, request);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadFromJsonAsync<Ingredient>() ?? throw new("No response");
+    }
+
+    public async Task<Ingredient[]> GetAllAsync()
+    {
+        var response = await client.GetFromJsonAsync<Ingredient[]>(Path);
+        return response ?? throw new("No response");
+    }
+
+    public Task<Ingredient?> GetAsync(GetIngredientRequest request)
+    {
+        return client.GetFromJsonAsync<Ingredient?>(Path.AppendPathSegment(request.Id));
+    }
+}
